9aa1543db8a75be5e06dbaf614c82e36f5b9aca1
Tamirys <tamygemeas@gmail.com>
2020-07-15 23:37:22 -0300

    modificações na estrutura

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express(); //instancia do express
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
 
 app.use(morgan('dev')); //retorna um log
+app.use(bodyParser.urlencoded({extended: false})); //apenas dados simples
+app.use(bodyParser.json()); //json de entrada
+
+app.use((req, res, next) => {
+    res.header('Acess-Control-Allow-Origin', '*');
+    res.header('Acess-Control-Allow-Header', 
+    'Origin, X-Rquested-With, Content-Type, Accept, Authorization');
+
+    if(res.method === 'OPTIONS'){
+        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 /*app.use('/teste', (req, res, next) => {
