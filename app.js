bd5d9d07d951314b5cb866c3511258029c63dc28
Tamirys <tamygemeas@gmail.com>
2020-07-15 21:26:14 -0300

    rotas e tratamento de erros

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express(); //instancia do express
+const morgan = require('morgan');
 
-app.use((req, res, next) => {
+const rotaProdutos = require('./routes/produtos');
+const rotaPedidos = require('./routes/pedidos');
+
+app.use(morgan('dev')); //retorna um log
+app.use('/produtos', rotaProdutos);
+app.use('/pedidos', rotaPedidos);
+/*app.use('/teste', (req, res, next) => {
     res.status(200).send({
         mensagem: 'Ok!'
+    });
+});*/
+
+//Nao encontra nenhuma rota
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
     });
 });
 
